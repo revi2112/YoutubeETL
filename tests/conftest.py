cdaff711c1879265e6b9e4e6d228b41b402d4ab9
Revathi <revathibhojanapalli20@gmail.com>
2026-08-27 12:22:02 -0500

    integration test

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,3 +36,36 @@ def mock_postgres_conn_var():
 @pytest.fixture
 def dagbag():
     yield DagBag()
+
+@pytest.fixture()
+def airflow_variable():
+    def get_airflow_variable(variable_name):
+        env_var = f"AIRFLOW_VAR_{variable_name.upper()}"
+        return os.getenv(env_var)
+
+    return get_airflow_variable
+
+
+@pytest.fixture
+def real_postgres_connection():
+    dbname = os.getenv("ELT_DATABASE_NAME")
+    user = os.getenv("ELT_DATABASE_USERNAME")
+    password = os.getenv("ELT_DATABASE_PASSWORD")
+    host = os.getenv("POSTGRES_CONN_HOST")
+    port = os.getenv("POSTGRES_CONN_PORT")
+
+    conn = None
+
+    try:
+        conn = psycopg2.connect(
+            dbname=dbname, user=user, password=password, host=host, port=port
+        )
+
+        yield conn
+
+    except psycopg2.Error as e:
+        pytest.fail(f"Failed to connect to the database: {e}")
+
+    finally:
+        if conn:
+            conn.close()
