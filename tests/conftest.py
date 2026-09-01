3e10e62e14ba549ddf5be0d438fda3a5929783a8
Revathi <revathibhojanapalli20@gmail.com>
2026-09-01 00:42:40 -0500

    variable

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 import pytest
 import psycopg2
 from unittest import mock
-from airflow.models import Varible, Connection, DagBag
+from airflow.models import Variable, Connection, DagBag
 
 @pytest.fixture
 def api_key():
@@ -15,7 +15,7 @@ def api_key():
 @pytest.fixture      
 def channel_handle():
     with mock.patch.dict("os.envrion", AIRFLOW_VAR_CHANNEL_HANDLE="MRCHEESE"):
-        yield Varible.get("CHANNEL_HANDLE")
+        yield Variable.get("CHANNEL_HANDLE")
         
 @pytest.fixture
 def mock_postgres_conn_var():
